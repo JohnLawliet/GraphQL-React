ae1b36c9e4a6a8917ffed3ba4ede56b466397ed0
johnlawliet <t.shiladitya@gmail.com>
2020-12-23 16:10:56 +0530

    Heroku deployment...

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use(
   }),
 );
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req,res) => {
-  res.sendFile(path.resolve(__dirname,'public','index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 const PORT = process.env.PORT || 5000
