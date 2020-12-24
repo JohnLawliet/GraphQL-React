c2726c660c7ca5943d97e50c7ebc280baf645514
johnlawliet <t.shiladitya@gmail.com>
2020-12-24 11:17:51 +0530

    Changed package.json scripts

--- server.js
+++ server.js
@@ -16,14 +16,16 @@ app.use(
   }),
 );
 
-app.use(express.static(path.join(__dirname, 'public')));
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+  app.get('*',function(req,res) {
+      res.sendFile(path.join(__dirname,'client/build','index.html'))
+  })
+}
 
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
  
-app.listen(PORT, () => {
-    console.log(`Working on port ${PORT}`)
+app.listen(port, () => {
+    console.log(`Working on port ${port}`)
 }); 
